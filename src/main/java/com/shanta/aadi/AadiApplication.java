4668037975b19de0488cfd7505833fa33c76a03a
Swagat Panta <you@example.com>
2024-07-11 17:25:53 +0545

    Initial commit

---
+++ src/main/java/com/shanta/aadi/AadiApplication.java
@@ -0,0 +1,13 @@
+package com.shanta.aadi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AadiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AadiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shanta/aadi/AadiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shanta.aadi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AadiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
